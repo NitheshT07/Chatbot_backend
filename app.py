1da18e724e0ff370fed378bf4e14d930944c8ffa
Nithesh T <155750335+NitheshT07@users.noreply.github.com>
2025-07-12 06:57:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,8 @@ def teach():
         json.dump(memory, file)
     return jsonify({"status": "learned", "question": question})
 
-if __name__ == "__main__":
-    app.run(port=5000)
+
+   if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
